68e6a23aa5755a0b352bc3524b62534e203f8144
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-09-12 09:45:01 +0530

    tpapi1 changes

--- blog.js
+++ blog.js
@@ -1,31 +1,41 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(function (response) {
-        return response.json();
-    })
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (products) {
+    products.forEach((product) => {
+      product.image = "images/blog/3.jpg";
+    });
 
-    .then(function (products) {
-        let output = document.querySelector(".blog-wrap");
-        out=""
-        products.forEach(product => {
-            product.image = 'images/blog/3.jpg'
-        });
+    let output = document.querySelector(".blog-wrap");
+    out = "";
+    let myobject = {
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+      id: 101,
+      image: "images/blog/4.jpg",
+      title: "qui est esse",
+      userId: 10,
+    };
+    products.push(myobject);
 
-       
-      
-       let chunkSize = 2;
-        for (let i = 0; i < products.length; i += chunkSize) {
-           
-            var splitArray = products.slice(i, i + chunkSize);
-        // console.log(splitArray)
-           
+    console.log("products ----", products);
+    let lcldata = JSON.stringify(products);
+    localStorage.setItem("data", lcldata);
 
-            for(let j = 0 ; j < splitArray.length; j++) {
-               
-                if(j==0) {
-                   
-                    out += '<div class="container"><div class="row">'
-                } 
-                out += `<div class="col-lg-6 col-md-6 mb-5">
+    let lclgetdata = JSON.parse(localStorage.getItem("data"));
+    let content = JSON.stringify(lclgetdata);
+    console.log(content, "updte");
+    localStorage.setItem("data", content);
+    let chunkSize = 3;
+    for (let i = 0; i < products.length; i += chunkSize) {
+      var splitArray = products.slice(i, i + chunkSize);
+      // console.log(splitArray)
+
+      for (let j = 0; j < splitArray.length; j++) {
+        if (j == 0) {
+          out += '<div class="container"> <div class="row">   ';
+        }
+        out += `<div class="col-lg-4 col-md-4 mb-5">
                 <div class="blog-item">
                     <img src="${splitArray[j].image}" alt="" class="img-fluid rounded" />
 
@@ -45,16 +55,10 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                 </div>
                 `;
 
-                if(j === splitArray.length- 1 )  {
-
-                   
-                    out += `</div></div>`;
-                }
-                
-                output.innerHTML = out;
-            
-    }}
-    })
-        
-
-
+        if (j === splitArray.length - 1) {
+          out += `</div></div>`;
+        }
+        output.innerHTML = out;
+      }
+    }
+  });
